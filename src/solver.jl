3cb5caf7cd19d9401bd40888ae0d2728290177e6
Edward Balaban <ebalaban@stanford.edu>
2017-02-13 12:51:20 -0800

    Removed transition and observation distribution type parameters

--- src/solver.jl
+++ src/solver.jl
@@ -1,5 +1,5 @@
 
-type DESPOTSolver{S,A,O,L,U,TD,OD} <: POMDPs.Solver
+type DESPOTSolver{S,A,O,L,U} <: POMDPs.Solver
     belief::DESPOTBelief{S,A,O}
     lb::L
     ub::U
@@ -9,8 +9,6 @@ type DESPOTSolver{S,A,O,L,U,TD,OD} <: POMDPs.Solver
     node_count::Int64
     config::DESPOTConfig
     #preallocated for simulations
-    # transition_distribution::TD
-    # observation_distribution::OD
     rng::DESPOT.DESPOTRandomNumber
     curr_reward::DESPOTReward
     next_state::S
